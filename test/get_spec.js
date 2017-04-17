7dcbeafcae6ff6f9bd7646bd6abf1129ef156add
Amin <mohamed.amin.088@gmail.com>
2017-04-18 01:47:30 +0400

    faster get

--- test/get_spec.js
+++ test/get_spec.js
@@ -4,6 +4,7 @@ var someData = {
 	data: {
 		user: {
 			id:1,
+			list:['item',1,2],
 			name: 'Amin',
 			details: {
 				school:{
@@ -15,6 +16,7 @@ var someData = {
 		}
 	}
 } 
+var list = [10, 20, 30]
 describe("get", function(){
 	it("gets shallow prop based on dot notation string", function(){
 		expect(function(){ return get('lalaland') }).toThrow(new Error('the source is not valid'));
@@ -26,5 +28,9 @@ describe("get", function(){
 
 	it("returns undefined if its not there", function(){
 		expect(get(someData, 'data.user.id.bla.lulu.any')).toBe(undefined);
+	});
+
+	it("Can access array", function(){
+		expect(get(someData, 'data.user.list.0')).toBe('item');
 	});
 });
